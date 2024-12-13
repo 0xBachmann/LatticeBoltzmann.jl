7e00801dab64c2ceb3e7e5eaeea9a4ff52b481dc
Jonas Bachmann <jbachmann@ethz.ch>
2024-12-13 14:51:44 +0100

    make all functions parallel

--- scripts/lb_3D_xpu.jl
+++ scripts/lb_3D_xpu.jl
@@ -123,86 +123,101 @@ end
     end
 end
 
-@views function periodic_boundary_update!(dimension, pop, pop_buf)
+@parallel_indices (i, j) function periodic_boundary_update!(dimension, pop, pop_buf)
     if dimension == :x
         Nx = size(pop, 1)
-        for j in axes(pop, 2)
-            for k in axes(pop, 3)
-                for q in 1:Q
-                    pop_buf[1, j, k, q] = pop[Nx-1, j, k, q]
-                    pop_buf[Nx, j, k, q] = pop[2, j, k, q]
-                end   
-            end         
-        end
+        for q in 1:Q
+            pop_buf[1, i, j, q] = pop[Nx-1, i, j, q]
+            pop_buf[Nx, i, j, q] = pop[2, i, j, q]
+        end   
     elseif dimension == :y
         Ny = size(pop, 2)
-        for i in axes(pop, 1)
-            for k in axes(pop, 3)
-                for q in 1:Q
-                    pop_buf[i, 1, k, q] = pop[i, Ny-1, k, q]
-                    pop_buf[i, Ny, k, q] = pop[i, 2, k, q]
-                end    
-            end        
-        end
+        for q in 1:Q
+            pop_buf[i, 1, j, q] = pop[i, Ny-1, j, q]
+            pop_buf[i, Ny, j, q] = pop[i, 2, j, q]
+        end    
     elseif dimension == :z
         Nz = size(pop, 3)
-        for i in axes(pop, 1)
-            for j in axes(pop, 2)
-                for q in 1:Q
-                    pop_buf[i, j, 1, q] = pop[i, j, Nz-1, q]
-                    pop_buf[i, j, Nz, q] = pop[i, j, 2, q]
-                end
-            end
+        for q in 1:Q
+            pop_buf[i, j, 1, q] = pop[i, j, Nz-1, q]
+            pop_buf[i, j, Nz, q] = pop[i, j, 2, q]
         end
     end
+    return
 end
 
-@views function bounce_back_boundary!(dimension, pop, pop_buf)
+
+@parallel_indices (i, j) function inlet_boundary_conditions!(dimension, pop, pop_buf, U_inlet, values)
     if dimension == :x
         lx = 2
         rx = size(pop, 1) - 1
-        for j in axes(pop, 2)[2:end-1]
-            for k in axes(pop, 3)[2:end-1]
-                for q in 1:Q
-                    if directions[q][1] == 1
-                        pop_buf[rx, j, k, q] = pop[rx, j, k, (q%2 == 0) ? q+1 : q-1]
-                    end
-                    if directions[q][1] == -1
-                        pop_buf[lx, j, k, q] = pop[lx, j, k, (q%2 == 0) ? q+1 : q-1]
-                    end
-                end   
-            end         
-        end
+        for q in 1:Q
+            if directions[q][1] == 1
+                pop_buf[rx, i, j, q] = pop[rx, i, j, (q%2 == 0) ? q+1 : q-1] - 2 * _cs2 * weights[q] * values[rx - 1, i - 1, j - 1] * dot(U_inlet, directions[q])
+            elseif directions[q][1] == -1
+                pop_buf[lx, i, j, q] = pop[lx, i, j, (q%2 == 0) ? q+1 : q-1] - 2 * _cs2 * weights[q] * values[lx - 1, i - 1, j - 1] * dot(U_inlet, directions[q])
+            end
+        end   
     elseif dimension == :y
         ly = 2
         ry = size(pop, 2) - 1
-        for i in axes(pop, 1)[2:end-1]
-            for k in axes(pop, 3)[2:end-1]
-                for q in 1:Q    
-                    if directions[q][2] == 1
-                        pop_buf[i, ry, k, q] = pop[i, ry, k, (q%2 == 0) ? q+1 : q-1]
-                    end
-                    if directions[q][2] == -1
-                        pop_buf[i, ly, k, q] = pop[i, ly, k, (q%2 == 0) ? q+1 : q-1]
-                    end
-                end    
-            end        
+        for q in 1:Q    
+            # if directions[q][2] == 1
+            #     pop_buf[i, ry, j, q] = pop[i, ry, j, (q%2 == 0) ? q+1 : q-1] - 2 * _cs2 * weights[q] * values[i - 1, ry - 1, j - 1] * dot(U_inlet, directions[q])
+            # else
+            if directions[q][2] == -1
+                pop_buf[i, ly, j, q] = pop[i, ly, j, (q%2 == 0) ? q+1 : q-1] - 2 * _cs2 * weights[q] * values[i - 1, ly - 1, j - 1] * dot(U_inlet, directions[q])
+            end    
         end
     elseif dimension == :z
         lz = 2
         rz = size(pop, 3) - 1
-        for i in axes(pop, 1)[2:end-1]
-            for j in axes(pop, 2)[2:end-1]
-                for q in 1:Q
-                    if directions[q][3] == 1 
-                        pop_buf[i, j, rz, q] = pop[i, j, rz, (q%2 == 0) ? q+1 : q-1]
-                    elseif directions[q][3] == -1
-                        pop_buf[i, j, lz, q] = pop[i, j, lz, (q%2 == 0) ? q+1 : q-1]
-                    end
-                end
+        for q in 1:Q
+            if directions[q][3] == 1 
+                pop_buf[i, j, rz, q] = pop[i, j, rz, (q%2 == 0) ? q+1 : q-1] - 2 * _cs2 * weights[q] * values[i - 1, j - 1, rz - 1] * dot(U_inlet, directions[q])
+            elseif directions[q][3] == -1
+                pop_buf[i, j, lz, q] = pop[i, j, lz, (q%2 == 0) ? q+1 : q-1] - 2 * _cs2 * weights[q] * values[i - 1, j - 1, lz - 1] * dot(U_inlet, directions[q])
             end
         end
     end
+    return
+end
+
+@parallel_indices (i, j) function bounce_back_boundary!(dimension, pop, pop_buf)
+    if dimension == :x
+        lx = 2
+        rx = size(pop, 1) - 1
+        for q in 1:Q
+            if directions[q][1] == 1
+                pop_buf[rx, i, j, q] = pop[rx, i, j, (q%2 == 0) ? q+1 : q-1]
+            end
+            if directions[q][1] == -1
+                pop_buf[lx, i, j, q] = pop[lx, i, j, (q%2 == 0) ? q+1 : q-1]
+            end
+        end   
+    elseif dimension == :y
+        ly = 2
+        ry = size(pop, 2) - 1
+        for q in 1:Q    
+            if directions[q][2] == 1
+                pop_buf[i, ry, j, q] = pop[i, ry, j, (q%2 == 0) ? q+1 : q-1]
+            end
+            if directions[q][2] == -1
+                pop_buf[i, ly, j, q] = pop[i, ly, j, (q%2 == 0) ? q+1 : q-1]
+            end    
+        end
+    elseif dimension == :z
+        lz = 2
+        rz = size(pop, 3) - 1
+        for q in 1:Q
+            if directions[q][3] == 1 
+                pop_buf[i, j, rz, q] = pop[i, j, rz, (q%2 == 0) ? q+1 : q-1]
+            elseif directions[q][3] == -1
+                pop_buf[i, j, lz, q] = pop[i, j, lz, (q%2 == 0) ? q+1 : q-1]
+            end
+        end
+    end
+    return
 end
 
 @parallel_indices (i, j, k) function streaming!(pop, pop_buf)
@@ -212,63 +227,48 @@ end
     return
 end
 
-@views function init!(velocity, density, temperature, U_init, R)
-    density .= 1
-    temperature .= 0
+@parallel_indices (i, j, k) function init!(velocity, density, temperature, U_init, R)
+    density[i, j, k] = 1
 
     Nx = size(velocity, 1)
     Ny = size(velocity, 2)
 
-    for i in axes(velocity, 1)
-        for j in axes(velocity, 2)
-            for k in axes(velocity, 3)
-                if ((i - Nx/2)^2 + (j - Ny/3)^2) < R^2
-                    velocity[i, j, k, :] = @zeros(3)
-                    temperature[i, j, k] = 1
-                else 
-                    velocity[i, j, k, :] = U_init
-                end
-            end
-        end
+    if ((i - Nx/2)^2 + (j - Ny/3)^2) < R^2
+        velocity[i, j, k, :] = @zeros(3)
+        temperature[i, j, k] = 1
+    else 
+        velocity[i, j, k, :] = U_init
+        temperature[i, j, k] = 0
     end
+    return
 end
 
-@views function update_moments!(velocity, density, temperature, density_pop, temperature_pop)
-    for i in axes(velocity, 1)
-        for j in axes(velocity, 2)
-            for k in axes(velocity, 3)
-                cell_density = 0
-                cell_velocity = @zeros(3)
-                cell_temperature = 0
-                for q in 1:Q
-                    cell_density += density_pop[i + 1, j + 1, k + 1, q]
-                    cell_temperature += temperature_pop[i + 1, j + 1, k + 1, q]
-                    cell_velocity += directions[q] * density_pop[i + 1, j + 1, k + 1, q]
-                end
-
-                cell_velocity /= cell_density
-                velocity[i, j, k, :] = cell_velocity
-                density[i, j, k] = cell_density
-                temperature[i, j, k] = cell_temperature
-            end
-        end
+@parallel_indices (i, j, k) function update_moments!(velocity, density, temperature, density_pop, temperature_pop)
+    cell_density = 0
+    cell_velocity = @zeros(3)
+    cell_temperature = 0
+    for q in 1:Q
+        cell_density += density_pop[i + 1, j + 1, k + 1, q]
+        cell_temperature += temperature_pop[i + 1, j + 1, k + 1, q]
+        cell_velocity += directions[q] * density_pop[i + 1, j + 1, k + 1, q]
     end
+
+    cell_velocity /= cell_density
+    velocity[i, j, k, :] = cell_velocity
+    density[i, j, k] = cell_density
+    temperature[i, j, k] = cell_temperature
+    return
 end
 
-@views function apply_external_force!(velocity, R)
+@parallel_indices (i, j, k) function apply_external_force!(velocity, R)
 
     Nx = size(velocity, 1)
     Ny = size(velocity, 2)
     
-    for i in axes(velocity, 1)
-        for j in axes(velocity, 2)
-            for k in axes(velocity, 3)
-                if ((i - Nx/2)*(i - Nx/2) + (j - Ny/3)*(j - Ny/3)) < R^2
-                    velocity[i, j, k, :] = @zeros(3)
-                end
-            end
-        end
+    if ((i - Nx/2)*(i - Nx/2) + (j - Ny/3)*(j - Ny/3)) < R^2
+        velocity[i, j, k, :] = @zeros(3)
     end
+    return
 end
 
 @views function f_eq(q, velocity, density)
@@ -278,16 +278,11 @@ end
     return weights[q] * density * (1. + uc * _cs2 + 0.5 * uc2 * _cs4 - 0.5 * u2 * _cs2)
 end
 
-@views function init_pop!(pop, velocity, values)
-    for i in axes(pop, 1)[2:end-1]
-        for j in axes(pop, 2)[2:end-1]
-            for k in axes(pop, 3)[2:end-1]
-                for q in 1:Q
-                    pop[i, j, k, q] = f_eq(q, velocity[i-1, j-1, k-1, :], values[i-1, j-1, k-1])
-                end
-            end
-        end
+@parallel_indices (i, j, k) function init_pop!(pop, velocity, values)
+    for q in 1:Q
+        pop[i, j, k, q] = f_eq(q, velocity[i-1, j-1, k-1, :], values[i-1, j-1, k-1])
     end
+    return
 end
 
 function lb()
@@ -324,43 +319,40 @@ function lb()
     U_init[2] = 0.2
 
     do_vis = true
-    nvis = 5
+    nvis = 10
     anim = Animation()
-    st = ceil(Int, Nx / 20)
+    st = ceil(Int, Nx / 15)
     Xc, Yc = [x for x in xc, _ in yc], [y for _ in xc, y in yc]
     Xp, Yp = Xc[1:st:end, 1:st:end], Yc[1:st:end, 1:st:end]
 
-    init!(velocity, density, temperature, U_init, R)
+    @parallel (1:Nx, 1:Ny, 1:Nz) init!(velocity, density, temperature, U_init, R)
+    
+    @parallel (2:Nx+1, 2:Ny+1, 2:Nz+1) init_pop!(density_pop, velocity, density)
+    @parallel (2:Nx+1, 2:Ny+1, 2:Nz+1) init_pop!(temperature_pop, velocity, temperature)
 
-    init_pop!(density_pop, velocity, density)
-    init_pop!(temperature_pop, velocity, temperature)
-    # periodic_boundary_update!(:x, density_pop, density_buf)
-    # periodic_boundary_update!(:x, temperature_pop, temperature_buf)
-    # periodic_boundary_update!(:y, density_pop, density_buf)
-    # periodic_boundary_update!(:y, temperature_pop, temperature_buf)
-    # periodic_boundary_update!(:z, density_pop, density_buf)
-    # periodic_boundary_update!(:z, temperature_pop, temperature_buf)
+    @parallel (1:Ny+2, 1:Nz+2) periodic_boundary_update!(:x, density_pop, density_buf)
+    @parallel (1:Ny+2, 1:Nz+2) periodic_boundary_update!(:x, temperature_pop, temperature_buf)
+    @parallel (1:Nx+2, 1:Nz+2) periodic_boundary_update!(:y, density_pop, density_buf)
+    @parallel (1:Nx+2, 1:Nz+2) periodic_boundary_update!(:y, temperature_pop, temperature_buf)
 
     for i in 1:nt
-        apply_external_force!(velocity, R)
+        @parallel (1:Nx, 1:Ny, 1:Nz) apply_external_force!(velocity, R)
 
         @parallel (2:Nx+1, 2:Ny+1, 2:Nz+1) collision!(density_pop, velocity, density, _τ_density)
         @parallel (2:Nx+1, 2:Ny+1, 2:Nz+1) collision!(temperature_pop, velocity, temperature, _τ_temperature)
-        # collision!(density_pop, velocity, density, _τ_density)
-        # collision!(temperature_pop, velocity, temperature, _τ_temperature)
 
         @parallel (2:Nx+1, 2:Ny+1, 2:Nz+1) streaming!(density_pop, density_buf)
         @parallel (2:Nx+1, 2:Ny+1, 2:Nz+1) streaming!(temperature_pop, temperature_buf)
 
-        periodic_boundary_update!(:x, density_pop, density_buf)
-        periodic_boundary_update!(:x, temperature_pop, temperature_buf)
-        periodic_boundary_update!(:y, density_pop, density_buf)
-        periodic_boundary_update!(:y, temperature_pop, temperature_buf)
-        periodic_boundary_update!(:z, density_pop, density_buf)
-        periodic_boundary_update!(:z, temperature_pop, temperature_buf)
+        # @parallel (1:Nx+2, 1:Nz+2) periodic_boundary_update!(:y, density_pop, density_buf)
+        # @parallel (1:Nx+2, 1:Nz+2) periodic_boundary_update!(:y, temperature_pop, temperature_buf)
+        # @parallel (2:Nx+1, 2:Nz+1) inlet_boundary_conditions!(:y, density_pop, density_buf, U_init, density)
+        # @parallel (2:Nx+1, 2:Nz+1) inlet_boundary_conditions!(:y, temperature_pop, temperature_buf, U_init, temperature)
+        @parallel (1:Nx+2, 1:Ny+2) periodic_boundary_update!(:z, density_pop, density_buf)
+        @parallel (1:Nx+2, 1:Ny+2) periodic_boundary_update!(:z, temperature_pop, temperature_buf)
 
-        bounce_back_boundary!(:x, density_pop, density_buf)
-        bounce_back_boundary!(:x, temperature_pop, temperature_buf)
+        @parallel (2:Ny+1, 2:Nz+1) bounce_back_boundary!(:x, density_pop, density_buf)
+        @parallel (2:Ny+1, 2:Nz+1) bounce_back_boundary!(:x, temperature_pop, temperature_buf)
         # bounce_back_boundary(:z, density_pop, density_buf)
         # bounce_back_boundary(:z, temperature_pop, temperature_buf)
         # dirichlet_boundary(:ylower, density_buf, U_init, density)
@@ -369,7 +361,7 @@ function lb()
         density_pop, density_buf = density_buf, density_pop
         temperature_pop, temperature_buf = temperature_buf, temperature_pop 
 
-        update_moments!(velocity, density, temperature, density_pop, temperature_pop)
+        @parallel (1:Nx, 1:Ny, 1:Nz) update_moments!(velocity, density, temperature, density_pop, temperature_pop)
 
         if do_vis && (i % nvis == 0)
             vel_c = copy(velocity[:, :, 1, 1:2])
